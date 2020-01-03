db9ea5f0a33c4d8bb1bfdb64fe68d72dde5ad46b
MndTheNerd <mndthenerd@gmail.com>
2020-01-04 01:09:49 +0300

    replacing the padding with FlatButton, including the onPressed property that shows a print

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('left button pressed!');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('right button pressed!');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           )
