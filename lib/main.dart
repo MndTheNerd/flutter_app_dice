a18b09c3ffb19b63c4cee79e5044cd6ab5ee0381
MndTheNerd <mndthenerd@gmail.com>
2020-01-04 16:22:26 +0300

    changing the StatlessWidget to SateFull widget and adding integer that can allow you to change between images

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,13 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int DiceNum1 = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,9 +30,12 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
+                setState(() {
+                  DiceNum1 = 5;
+                });
                 print('left button pressed!');
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$DiceNum1.png'),
             ),
           ),
           Expanded(
